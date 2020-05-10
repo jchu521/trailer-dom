692dc99d9421670d4cf75d45a522d3ea324e6084
Jonathan Chueh <jonathanchueh1992@gmail.com>
2020-05-10 22:59:44 +1000

    add search bar component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./SearchBar.scss";
+
+export default function SearchBar() {
+  return (
+    <div className="searchbar">
+      <div className="searchbar-container"></div>
+    </div>
+  );
+}
