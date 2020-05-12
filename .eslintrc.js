d3e87278823cc5966dfa56920bcdcd205c90245a
Jonathan Chueh <jonathanchueh1992@gmail.com>
2020-05-12 19:32:44 +1000

    add movie and TV apis, update header and searchbar, install async webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    semi: ["error", "always"],
+  },
 };
--- src/app/Header/Header.js
+++ src/app/Header/Header.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchBar from "../../components/SearchBar/SearchBar";
 
+import { getNowPlayingAPI } from "../../apis/Movies/index";
 import "./Header.scss";
 
 export default function Header() {
+  const fetchNowPlayingMovies = async () => {
+    let r = await getNowPlayingAPI();
+    console.log(r);
+    return r;
+  };
+
+  useEffect(() => {
+    fetchNowPlayingMovies();
+  }, []);
+
   return (
     <header className="header">
       <div className="header container">
         <div className="header row">
-          <span className="col-md-2 col-lg-5">Logo</span>
-          <div className="col-md-10 col-lg-7">131231231</div>
+          <div className="col-sm-8">Logo</div>
+          <div className="col-sm-1">Home</div>
+          <div className="col-sm-1">Movies</div>
+          <div className="col-sm-1">Series</div>
+          <div className="col-sm-1">Search</div>
         </div>
       </div>
       <SearchBar />
